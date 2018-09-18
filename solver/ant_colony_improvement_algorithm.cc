64ecc33f4708f0f3f891957c71ac2d773f158ec9
rols <znjrols5@gmail.com>
2018-09-18 12:23:45 +0200

    WIP ant colony

--- solver/ant_colony_improvement_algorithm.cc
+++ solver/ant_colony_improvement_algorithm.cc
@@ -1,5 +1,24 @@
 #include "ant_colony_improvement_algorithm.h"
 
+#include <memory>
+
+using std::make_shared;
+
 namespace TSP {
+
+void AntColonyImprovementAlgorithm::Reset() {
+
+  ants_.clear();
+
+  pheromones_ = make_shared<SquareMatrix<double>> (world_->size);
+}
+
+bool AntColonyImprovementAlgorithm::Iterate(int granularity) {
+  return false;
+}
+
+int AntColonyImprovementAlgorithm::GetMaxGranularity() {
+  return 0;
+}
 
 };// namespace TSP
--- solver/ant_colony_improvement_algorithm.h
+++ solver/ant_colony_improvement_algorithm.h
@@ -5,7 +5,19 @@
 namespace TSP {
 
 class AntColonyImprovementAlgorithm : public ImprovementAlgorithm {
+ public:
+  void Reset() override;
+  bool Iterate(int granularity) override;
+  int GetMaxGranularity() override;
+ private:
 
+  struct Ant {
+    unordered_set<int> unvisited_;
+    Path current_path_;
+  };
+
+  shared_ptr<SquareMatrix<double>> pheromones_;
+  vector<Ant> ants_;
 };
 
 } // namespace TSP
